f34fd14a25d171c59dac484004d89234d071f210
Hoyuo <psh9907@gmail.com>
2015-02-08 20:43:42 +0900

    1PC 2인용

--- app.js
+++ app.js
@@ -16,35 +16,61 @@ app.get('/', function (req, res) {
 var socket_ids = [];
 var count = 0;
 
-
-
 function registerUser(socket, nickname) {
     socket.get('nickname', function (err, pre_nick) {
         if (pre_nick != undefined) delete socket_ids[pre_nick];
-        socket_ids[nickname] = socket.id
+        socket_ids[nickname] = socket.id;
+        socket.set('nickname', nickname, function () {
+        });
     });
 }
 
 io.sockets.on('connection', function (socket) {
-    socket.emit('new', {nickname : 'player' + count});
+    socket.emit('new');
 
-    socket.on('btn', function(data) {
-        console.log("btn : " + data);
-        io.sockets.emit('btn', data);
+    socket.on('newSend', function (data) {
+        registerUser(socket, data);
+    });
 
+    socket.on('UUID', function (data) {
+        var nickname = 'player' + count;
+        count++;
+        count = count % 2;
+        registerUser(socket, nickname);
+        console.log(nickname);
+    });
+
+    socket.on('btn', function (data) {
+        socket.get('nickname', function (err, nickname) {
+            var iValue = nickname.indexOf('0');
+            var socket_id = socket_ids['webPage'];
+            if (socket_id != undefined) {
+                if (iValue != -1)
+                    io.sockets.socket(socket_id).emit('btn_2', data);
+                else
+                    io.sockets.socket(socket_id).emit('btn_1', data);
+            }// if
+        });
     });
 
     socket.on('pad', function(data, flag) {
-        console.log("pad : " + data + flag);
-        io.sockets.emit('pad', data, flag);
-
+        socket.get('nickname', function (err, nickname) {
+            var iValue = nickname.indexOf('0');
+            var socket_id = socket_ids['webPage'];
+            if (socket_id != undefined) {
+                if (iValue != -1)
+                    io.sockets.socket(socket_id).emit('pad_2', data, flag);
+                else
+                    io.sockets.socket(socket_id).emit('pad_1', data, flag);
+            }// if
+        });
     });
 
-    socket.on('UUID', function(data) {
-       console.log("UUID : " + data);
+    socket.on('disconnect', function () {
+        socket.get('nickname', function (err, nickname) {
+            if (nickname != undefined) {
+                delete socket_ids[nickname];
+            }
+        });
     });
-
-    socket.on('disconnection', function() {
-        console.log('disconnection');
-    })
 });
