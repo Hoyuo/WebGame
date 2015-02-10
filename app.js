5544c4b4c72b3ebd11115fa572a6beff7bb68787
Hoyuo <psh9907@gmail.com>
2015-02-11 01:55:38 +0900

    대격변

--- app.js
+++ app.js
@@ -1,17 +1,84 @@
-var sys = require('sys'),
-    http = require('http'),
-    express = require('express'),
-    app = express(),
-    server = http.createServer(app),
-    io = require('socket.io').listen(server);
+//http 서버
+var http = require('http');
+var express = require('express');
+var routes = require('./routes');
+var app = express();
+var server = http.createServer(app);
+server.listen(3000);
 
-server.listen(13000);
+//웹페이지 구성
+var engine = require('ejs-locals');
+var path = require('path');
+var bodyParser = require('body-parser');
+var session = require('cookie-session');
+var favicon = require('serve-favicon');
 
-app.use(express.static(__dirname + '/public'));
+//Socket.IO
+var io = require('socket.io').listen(server);
 
-app.get('/', function (req, res) {
-    res.send('Hello World');
-});
+//비밀번호 암호화
+var crypto = require('crypto');
+var myHash = function myHash(key) {
+    var hash = crypto.createHash('sha1');
+    hash.update(key);
+    return hash.digest('hex');
+}
+
+//Create Session
+var createSession = function createSession() {
+    return function (req, res, next) {
+        if (!req.session.login) {
+            req.session.login = 'logout';
+        }
+        next();
+    };
+};
+
+//all environment
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(require('express-method-override')('method_override_param_name'));
+app.use(session({secret: 'secret key'}));
+app.use(express.static(path.join(__dirname, '/views')));
+
+app.engine('ejs', engine);
+
+app.get('/', routes.index);
+app.get('/SIGN_UP', routes.sign_up);
+app.get('/CHECKUSERNAME', routes.checkUserName);
+app.get('/LOGOUT', routes.logout);
+app.get('/JSNES', routes.jsnes);
+app.get('/gameroomlist', routes.gameroomlist);
+app.get('/CREATEROOM', routes.createroom);
+
+app.post('/SIGN_UP', function (req, res, next) {
+    if (req.body.password == req.body.confirm_password) {
+        req.username = req.body.username;
+        req.password = myHash(req.body.password);
+        next();
+    }
+    else {
+        res.redirect('/SIGN_UP');
+    }
+}, routes.sign_up_post);
+
+app.post('/LOGIN', function (req, res, next) {
+    req.username = req.body.username;
+    req.password = myHash(req.body.password);
+    next();
+}, routes.login_post);
+
+app.post('/LOGINMOBILE', function (req, res, next){
+    req.username = req.body.userId;
+    req.password =  myHash(req.body.password);
+    console.log(req.username);
+    console.log(req.password);
+    next();
+}, routes.moblie_login_post);
 
 var socket_ids = [];
 var count = 0;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,168 @@
-var express = require('express');
-var router = express.Router();
+//DB Connection
+var mongoose = require('mongoose');
+var url = require('url');
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+//connects to MongoDB
+mongoose.connect('mongodb://210.118.74.103:27017/membership');
+
+//get the connection from mongoose
+var db = mongoose.connection;
+
+//creates DB schma for MongoDB
+var memberSchema = mongoose.Schema({
+    username: 'string',
+    password: 'string'
 });
 
-module.exports = router;
+//compiles our schema into model
+var Member = mongoose.model('Member', memberSchema);
+
+//route functions
+// Main (page: 0)
+exports.index = function (req, res) {
+    if (req.session.login === 'login') {
+        res.redirect('/gameroomlist');
+        return;
+    }
+    res.status(200);
+    res.render('index', {
+        title: 'OldGame',
+        page: 0,
+        url: req.url,
+        login: req.session.login,
+        username: req.session.username
+    });
+};
+
+exports.login_post = function (req, res) {
+    res.status(200);
+    Member.findOne({username: req.username, password: req.password}, function (err, member) {
+        if (member != null) {
+            req.session.login = 'login';
+            req.session.username = req.username;
+            res.status(200);
+            res.redirect('/gameroomlist');
+            console.log('로그인 성공');
+        }
+        else {
+            res.status(200);
+            res.redirect('/');
+        }
+    });
+};
+
+exports.gameroomlist = function (req, res) {
+    if (req.session.login !== 'login') {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('gameroomlist', {
+        title: 'OldGame',
+        login: req.session.login,
+        username: req.session.username,
+        url: req.url
+    });
+}
+
+exports.moblie_login_post = function (req, res) {
+    Member.findOne({username: req.username, password: req.password}, function (err, member) {
+        if (member != null) {
+            res.json({status: 200, username: req.username});
+        }
+        else {
+            res.json({status: 100, username: req.username});
+        }
+    });
+};
+
+exports.logout = function (req, res) {
+    req.session.login = 'logout';
+
+    res.status(200);
+    res.redirect('/');
+};
+
+exports.sign_up = function (req, res) {
+    res.status(200);
+
+    res.render('sign_up', {
+        title: 'Sign up',
+        url: req.url,
+        page: 5,
+        login: req.session.login,
+        username: req.session.username,
+        existingUsername: 'null'
+    });
+
+};
+
+exports.checkUserName = function (req, res) {
+    var uri = url.parse(req.url, true);
+    Member.findOne({username: uri.query.id}, function (err, member) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        if (member != null) {
+            res.end('true');
+        }
+        else {
+            res.end('false');
+        }
+    });
+}
+
+exports.sign_up_post = function (req, res) {
+    res.status(200);
+
+    var curUsername = req.username;
+    if (curUsername == "") {
+        res.redirect('/SIGN_UP');
+    } else {
+        Member.findOne({username: curUsername}, function (err, member) {
+            if (err) return handleError(err);
+
+            if (member == null) {
+                var myMember = new Member({username: curUsername, password: req.password});
+                myMember.save(function (err, data) {
+                    if (err) {// TODO handle the error
+                        console.log("error");
+                    }
+                    console.log('member is inserted');
+                });
+                res.redirect('/');
+            }
+            else {
+                res.redirect('/SIGN_UP');
+            }
+        });
+    }
+};
+
+exports.jsnes = function (req, res) {
+    if (req.session.login !== 'login') {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('jsnes', {
+        title: 'OldGame',
+        url: req.url,
+        page: 6,
+        login: req.session.login,
+        username: req.session.username
+    });
+};
+
+exports.createroom = function(req, res) {
+    if (req.session.login !== 'login') {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('createroom', {
+        title: 'OldGame',
+        url: req.url,
+        page: 7,
+        login: req.session.login,
+        username: req.session.username
+    });
+}
