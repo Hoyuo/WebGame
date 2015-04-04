d4edac285733a55293b4703a8556db9efe75811b
Hoyuo <psh9907@gmail.com>
2015-04-05 03:39:42 +0900

    소스정리

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ var http = require('http');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-//var logger = require('morgan');
-//var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
@@ -18,24 +16,17 @@ var url = require('url');
 
 var io = require('socket.io').listen(server);
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-//app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(express.cookieParser());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, '/views')));
 app.set('view engine', 'ejs');
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(require('express-method-override')('method_override_param_name'));
 app.use(session({secret: 'keyboard cat', resave: false, saveUninitialized: true}));
-//app.use(createSession());
 app.engine('ejs', engine);
 
 app.use('/', routes);
@@ -46,66 +37,56 @@ io.set('log level', 2);
 
 var socket_user_id = [];
 
-io.sockets.on('connection', function(socket){
-    socket.on('save_user_id', function(data){
-       socket_user_id[socket.id] = data.user_id;
+io.sockets.on('connection', function (socket) {
+    socket.on('save_user_id', function (data) {
+        socket_user_id[socket.id] = data.user_id;
     });
 
-    socket.on('set_1p_peer_id', function(data){
+    socket.on('set_1p_peer_id', function (data) {
         routes.roomList[data.roomNum].peer_1p_id = data.peer_1p_id;
         //console.log(routes.roomList);
     });
 
-    socket.on('set_2p_peer_id', function(data){
+    socket.on('set_2p_peer_id', function (data) {
         routes.roomList[data.roomNum].peer_2p_id = data.peer_2p_id;
         //console.log(routes.roomList);
         console.log(routes.roomList[data.roomNum]);
     });
 
-    socket.on('disconnect', function(){
+    socket.on('disconnect', function () {
         //console.log(socket_user_id[socket.id]);
-        for(var i=0; i<routes.loginUser.length; i++){
-            if( routes.loginUser[i] == socket_user_id[socket.id] ){
+        for (var i = 0; i < routes.loginUser.length; i++) {
+            if (routes.loginUser[i] == socket_user_id[socket.id]) {
                 delete routes.loginUser[i];
                 break;
             }
         }
-        //console.log(routes.loginUser.length + '?' + routes.loginCnt + ' user login : ' + routes.loginUser);
+    });
+
+    //Android Processing
+    socket.on('btn', function (userId, btn) {
+        var room = routes.roomFind(userId);
+        console.log(pad, room);
+        if (room != null) {
+            if (routes.roomList[room].user_1p_id == userId) {
+                socket.emit('btn_1', btn);
+            } else if (routes.roomList[room].user_2p_id == userId) {
+                socket.emit('btn_2', btn);
+            }
+        }
+    });
+
+    socket.on('pad', function (userId, dir, flag) {
+        var room = routes.roomFind(userId);
+        console.log(pad, room);
+        if (room != null) {
+            if (routes.roomList[room].user_1p_id == userId) {
+                socket.emit('pad_1', dir, flag);
+            } else if (routes.roomList[room].user_2p_id == userId) {
+                socket.emit('pad_2', dir, flag);
+            }
+        }
     });
 });
-
-
-/*
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});*/
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,9 @@ var db = mongoose.connection;
 
 //creates DB schma for MongoDB
 var memberSchema = mongoose.Schema({
-  username: 'string',
-  password: 'string',
-  uuid: 'string'
+    username: 'string',
+    password: 'string',
+    uuid: 'string'
 });
 
 //compiles our schema into model
@@ -32,177 +32,187 @@ var loginUser = [];
 var loginCnt = 0;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  var errCode = req.query.errCode;
+router.get('/', function (req, res, next) {
+    var errCode = req.query.errCode;
 
-  if( errCode == null ){
-    errCode = 0;
-  }
-
-  if( req.session.user_id == null ) {
-    res.render('login', {
-      title: 'Return 1990 - Main',
-      login: 'logout',
-      username: '',
-      errCode: errCode
-    });
-  }
-  else{
-    res.redirect('/ROOM_LIST');
-  }
-});
-
-router.get('/SIGN_UP', function(req, res, next){
-  res.render('sign_up', {
-    title: 'Return 1990 - SignUp',
-    login: 'logout',
-    username: ''
-  });
-});
-
-router.get('/ROOM_LIST', function(req, res, next){
-  var errCode = req.query.errCode;
-
-  if( errCode == null ){
-    errCode = 0;
-  }
-
-  if( req.session.user_id == null ){
-    res.redirect('/');
-  }
-  else {
-    res.render('room_list', {
-      title: 'Return 1990 - GameRoomList',
-      login: 'login',
-      username: req.session.user_id,
-      roomList: roomList,
-      errCode: errCode
-    });
-  }
-});
-
-router.get('/CHECKROOMNAME', function(req, res, next){
-  var roomName = req.query.roomName;
-  var result = false;
-
-  for(var i=0; i<roomList.length; i++){
-    if( roomList[i].roomName == roomName ){
-      result = true;
+    if (errCode == null) {
+        errCode = 0;
     }
-  }
 
-  if( result == true ){
-    res.end('true');
-  }
-  else{
-    res.end('false');
-  }
+    if (req.session.user_id == null) {
+        res.render('login', {
+            title: 'Return 1990 - Main',
+            login: 'logout',
+            username: '',
+            errCode: errCode
+        });
+    }
+    else {
+        res.redirect('/ROOM_LIST');
+    }
 });
 
-router.get('/1P_OUT_ROOM', function(req, res, next){
-  var roomNum = req.query.roomNum;
+router.get('/SIGN_UP', function (req, res, next) {
+    res.render('sign_up', {
+        title: 'Return 1990 - SignUp',
+        login: 'logout',
+        username: ''
+    });
+});
 
-  delete roomList[roomNum];
+router.get('/ROOM_LIST', function (req, res, next) {
+    var errCode = req.query.errCode;
 
-  res.redirect('/ROOM_LIST');
+    if (errCode == null) {
+        errCode = 0;
+    }
+
+    if (req.session.user_id == null) {
+        res.redirect('/');
+    }
+    else {
+        res.render('room_list', {
+            title: 'Return 1990 - GameRoomList',
+            login: 'login',
+            username: req.session.user_id,
+            roomList: roomList,
+            errCode: errCode
+        });
+    }
+});
+
+router.get('/CHECKROOMNAME', function (req, res, next) {
+    var roomName = req.query.roomName;
+    var result = false;
+
+    for (var i = 0; i < roomList.length; i++) {
+        if (roomList[i].roomName == roomName) {
+            result = true;
+        }
+    }
+
+    if (result == true) {
+        res.end('true');
+    }
+    else {
+        res.end('false');
+    }
+});
+
+router.get('/1P_OUT_ROOM', function (req, res, next) {
+    var roomNum = req.query.roomNum;
+
+    delete roomList[roomNum];
+
+    res.redirect('/ROOM_LIST');
 });
 
 
 var crypto = require('crypto');
 var myHash = function myHash(key) {
-  var hash = crypto.createHash('sha1');
-  hash.update(key);
-  return hash.digest('hex');
+    var hash = crypto.createHash('sha1');
+    hash.update(key);
+    return hash.digest('hex');
 };
 
 
 /* POST Home Page */
-router.post('/LOGIN', function(req, res, next){
-  Member.findOne({username: req.body.user_id}, function (err, member) {
-    if( member === null ){
-      res.redirect('/?errCode=1');
-    }
-    else{
-      if( member.password !== myHash(req.body.user_pw) ){
-        res.redirect('/?errCode=2');
-      }
-      else{
-        var result = 0;
+router.post('/LOGIN', function (req, res, next) {
+    Member.findOne({username: req.body.user_id}, function (err, member) {
+        if (member === null) {
+            res.redirect('/?errCode=1');
+        }
+        else {
+            if (member.password !== myHash(req.body.user_pw)) {
+                res.redirect('/?errCode=2');
+            }
+            else {
+                var result = 0;
 
-        for(var i=0; i<loginUser.length; i++){
-          if( loginUser[i] == req.body.user_id ){
-            result = 1;
-          }
-        }
+                for (var i = 0; i < loginUser.length; i++) {
+                    if (loginUser[i] == req.body.user_id) {
+                        result = 1;
+                    }
+                }
 
-        if( result == 0 ){
-          req.session.user_id = req.body.user_id;
-          loginUser[loginCnt++] = req.body.user_id;
-          res.redirect('/ROOM_LIST');
+                if (result == 0) {
+                    req.session.user_id = req.body.user_id;
+                    loginUser[loginCnt++] = req.body.user_id;
+                    res.redirect('/ROOM_LIST');
+                }
+                else {
+                    res.redirect('/?errCode=3');
+                }
+            }
         }
-        else{
-          res.redirect('/?errCode=3');
-        }
-      }
-    }
-  });
+    });
 });
 
-router.post('/CREATE_ROOM', function(req, res, next){
-  res.render('create_room', {
-    title: 'Return 1990 - Create Room',
-    username: req.session.user_id,
-    login: 'login'
-  });
+router.post('/CREATE_ROOM', function (req, res, next) {
+    res.render('create_room', {
+        title: 'Return 1990 - Create Room',
+        username: req.session.user_id,
+        login: 'login'
+    });
 });
 
-router.post('/1P_GAME_ROOM', function(req, res, next){
-  roomList[roomSqu] = {
-    roomNum: roomSqu,
-    roomName: req.body.roomname,
-    gameName: req.body.insert_game,
-    maxPlayer: req.body.insert_player,
-    currentPlayer: 1,
-    peer_1p_id: null,
-    peer_2p_id: null,
-    user_1p_id: req.session.user_id,
-    user_2p_id: null
-  };
+router.post('/1P_GAME_ROOM', function (req, res, next) {
+    roomList[roomSqu] = {
+        roomNum: roomSqu,
+        roomName: req.body.roomname,
+        gameName: req.body.insert_game,
+        maxPlayer: req.body.insert_player,
+        currentPlayer: 1,
+        peer_1p_id: null,
+        peer_2p_id: null,
+        user_1p_id: req.session.user_id,
+        user_2p_id: null
+    };
 
-  roomSqu++;
+    roomSqu++;
 
-  res.render('1p_game_room', {
-    title: 'Return 1990 - 1PGameRoom',
-    login: 'login',
-    username: req.session.user_id,
-    roomInfo: roomList[roomSqu-1]
-  });
-});
-
-router.post('/2P_GAME_ROOM', function(req, res, next){
-  var room = roomList[req.body.roomNum];
-
-  if( room.maxPlayer == '1' ){
-    res.redirect('/ROOM_LIST?errCode=1');
-  }
-  else{
-    if( room.currentPlayer == 2 ){
-      res.redirect('/ROOM_LIST?errCode=2');
-    }
-    else{
-      room.currentPlayer = 2;
-      room.user_2p_id = req.session.user_id;
-
-      res.render('2p_game_room', {
-        title: 'Return 1990 - 2PGameRoom',
+    res.render('1p_game_room', {
+        title: 'Return 1990 - 1PGameRoom',
         login: 'login',
         username: req.session.user_id,
-        roomInfo: roomList[room.roomNum]
-      });
-    }
-  }
+        roomInfo: roomList[roomSqu - 1]
+    });
 });
+
+router.post('/2P_GAME_ROOM', function (req, res, next) {
+    var room = roomList[req.body.roomNum];
+
+    if (room.maxPlayer == '1') {
+        res.redirect('/ROOM_LIST?errCode=1');
+    }
+    else {
+        if (room.currentPlayer == 2) {
+            res.redirect('/ROOM_LIST?errCode=2');
+        }
+        else {
+            room.currentPlayer = 2;
+            room.user_2p_id = req.session.user_id;
+
+            res.render('2p_game_room', {
+                title: 'Return 1990 - 2PGameRoom',
+                login: 'login',
+                username: req.session.user_id,
+                roomInfo: roomList[room.roomNum]
+            });
+        }
+    }
+});
+
+var roomFind = function (userId) {
+    for (var room in roomList) {
+        if (userId == room.user_1p_id || userId == room.user_2p_id) {
+            return room;
+        }
+    }
+    return null;
+}
 
 module.exports = router;
 module.exports.roomList = roomList;
 module.exports.loginUser = loginUser;
+module.exports.roomFind = roomFind;
